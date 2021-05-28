31d3b9d557d47e4dae636803d6174b347f117e61
MitchLahm <mel64643@uga.edu>
2021-05-28 15:42:04 -0400

    More examples

---
+++ Example/1_H2O/Redundants/main.py
@@ -0,0 +1,16 @@
+from MixedHessian.options import Options
+options_kwargs = {
+  'queue'             : "gen3.q,gen4.q,gen6.q,debug.q",
+  'program'           : "psi4@master",
+  'energyRegex'       : r"Giraffe The Energy is\s+(\-\d+\.\d+)",
+  'cartInsert'        : 7,
+  'coords'            : "Redundant",
+  # 'calc'              : False,
+  'successRegex'      : r"beer"
+}
+options_obj = Options(**options_kwargs)
+
+# 3. call Mixed Hessian Program
+from MixedHessian.MH import MixedHessian
+MH_obj = MixedHessian(options_obj)
+MH_obj.run()
